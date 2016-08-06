c886dfc8ff885b5cd97ef978d25ddf7536c2511e
regainDeveloper <ntwarijoshua@gmail.com>
2016-08-06 14:02:24 +0200

    cors

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -45,7 +45,7 @@ class AuthenticationController extends Controller
             'code' => $request->input('code'),
             'client_id' => $request->input('clientId'),
             'redirect_uri' => $request->input('redirectUri'),
-            'client_secret' => Config::get('app.facebook_key')
+            'client_secret' => '1179632552056976'
         ];
 
         //Exchange Authorization code for Access Token.
