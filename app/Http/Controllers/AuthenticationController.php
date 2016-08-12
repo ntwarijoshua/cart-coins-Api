d436acbbfa31bd079af5491e2be3275020115ff1
regainDeveloper <ntwarijoshua@gmail.com>
2016-08-12 16:33:39 +0200

    test

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -81,7 +81,7 @@ class AuthenticationController extends Controller
             return response()->json(['token'=>$this->createToken($user)]);
         }else{
             //If user is not authenticated.
-            $fbuser = User::where('facebook_id',$facebookProfile['id']);
+            $fbuser = User::where('facebook_id',$facebookProfile->id);
             if($fbuser->first()){
                 return response()->json($fbuser);
                 //return response()->json(['token'=>$this->createToken($user)]);
